537d7b3aaafedd64df99f3a8d1c7edf063b2a08c
snelusha <hello@snelusha.dev>
2026-01-11 21:51:11 +0530

    feat: implement parser

---
+++ parser.go
@@ -0,0 +1,57 @@
+package bfgo
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
+func Parse(reader *bufio.Reader) ([]Op, error) {
+	ops := make([]Op, 0, 256)
+	stack := make([]int, 0, 32)
+
+	lexer := NewLexer(reader)
+
+	for {
+		op, err := lexer.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+
+		switch op.kind {
+		case OpJumpFwd:
+			stack = append(stack, len(ops))
+			ops = append(ops, op)
+
+		case OpJumpBack:
+			if len(stack) == 0 {
+				return nil, fmt.Errorf("unmatched ']' at operation %d", len(ops))
+			}
+
+			// pop from stack
+			openPos := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			ops[openPos].operand = uint(len(ops))
+
+			jumpBackOp, ok := NewOpWithOperand(']', uint(openPos))
+			if !ok {
+				return nil, fmt.Errorf("failed to create jump back operation at position %d", len(ops))
+			}
+
+			ops = append(ops, jumpBackOp)
+
+		default:
+			ops = append(ops, op)
+		}
+	}
+
+	if len(stack) != 0 {
+		return nil, fmt.Errorf("unmatched '[' at operation %d", stack[len(stack)-1])
+	}
+
+	return ops, nil
+}
